9bdfc6718dfa06aeb9bdd113deaa7f0b1dd7ba3f
Cholmes504 <161229132+Cholmes504@users.noreply.github.com>
2024-02-25 10:01:44 -0600

    Add files via upload

---
+++ ConvertString.java
@@ -0,0 +1,13 @@
+package practiceProblem;
+
+public class ConvertString {
+
+	public static void main(String [] args) {
+	
+		String greeting = "HELLO";
+		
+		String output = greeting.toLowerCase();
+		
+		System.out.println(output);
+	}	
+}
---
+++ FactorialFinder.java
@@ -0,0 +1,18 @@
+package practiceProblem;
+
+public class FactorialFinder {
+
+	public static void main(String[] args) {
+		System.out.println(factorial(10));
+		
+	}
+	public static int factorial (int n ) {
+		if(n == 1) {
+			return 1;
+		}	else {
+		
+		return n * factorial(n-1);
+		}
+		
+	}
+}
